f7341d3410c8aec6aee3a0669a490f503cb3e899
cahyo40 <mdcnugroho@gmail.com>
2022-12-21 23:16:36 +0700

    menambah helper tanggal

---
+++ lib/helper/date.dart
@@ -0,0 +1,20 @@
+import 'package:intl/intl.dart';
+
+class YoDate {
+  YoDate._();
+
+  /// return 12 December 2022
+  static String getDate(DateTime date) {
+    return ddmmmmy(date);
+  }
+
+  /// return 12 December 2022
+  static String ddmmmmy(DateTime date) {
+    return DateFormat("dd MMMM y", "id_ID").format(date);
+  }
+
+  /// example format "dd-mm-y"
+  static String getCustomDate(DateTime date, {required format}) {
+    return DateFormat(format).format(date);
+  }
+}
